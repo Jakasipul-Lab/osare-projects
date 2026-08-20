be1e95a6ce47bb360c75a1e4da05bf03603efe11
osare nakinson <owangochief@gmail.com>
2026-08-20 23:07:07 +0200

    Update import path for TierExplorer component

--- app/safari/page.js
+++ app/safari/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { TierExplorer } from '../page'
+import { TierExplorer } from '@/components/TierExplorer'
 
 export default function SafariPage() {
   return <TierExplorer type="safari" />
