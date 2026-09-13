818f6706bdc8568d8450495a33d486282c284317
osare nakinson <owangochief@gmail.com>
2026-09-13 16:21:30 +0200

    Add AdSense component to LocalPage

--- app/local/page.js
+++ app/local/page.js
@@ -1,6 +1,12 @@
 'use client'
 import { TierExplorer } from '@/components/TierExplorer'
+import { AdSense } from '@/components/AdSense'
 
 export default function LocalPage() {
-  return <TierExplorer type="local" />
+  return (
+    <>
+      <AdSense />
+      <TierExplorer type="local" />
+    </>
+  )
 }
