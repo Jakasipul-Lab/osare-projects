1db535ca4ea385fb6338c0c3845c2f8827ff656d
osare nakinson <owangochief@gmail.com>
2026-09-26 22:47:18 +0200

    Import LocationMap component in page.js

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import {
   Facebook, Linkedin, Youtube
 } from 'lucide-react'
 import AboutView from '@/components/AboutView'
+import LocationMap from '@/components/LocationMap'
 import { AdSense } from '@/components/AdSense'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
